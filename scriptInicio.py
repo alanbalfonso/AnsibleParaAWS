99860765d9130b82ef0c136c5ddcff68b0c4ba60
alanbalfonso <a.bauzaa27@gmail.com>
2026-03-23 00:21:42 -0600

    cambios de configuración

--- scriptInicio.py
+++ scriptInicio.py
@@ -1,7 +1,3 @@
 import platform
 
-print("Hola desde AWS Systems Manager")
 print("Sistema operativo:", platform.system())
-print("Versión:", platform.version())
-print("Release:", platform.release())
-print("Arquitectura:", platform.machine())
